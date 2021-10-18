9fe2103a0ee864c1960595435716accfc7cc3970
EnricoPf <65573747+EnricoPf@users.noreply.github.com>
2021-10-17 23:09:23 -0300

    Fixed sizes for main vectors

--- TP1/trilha_euleriana_principal.cpp
+++ TP1/trilha_euleriana_principal.cpp
@@ -82,9 +82,9 @@ int main(){
 	Grafo grafo = criarGrafo(file);
 
     //Inicializem as variáveis
-    int origem[999]; // Lista contendo os vértices de origem de cada aresta na trilha.
-    int destino[999]; // Lista contendo os vértices de destino de cada aresta na trilha.
-    int trilha[999]; // Lista contendo a ordem dos vértices na trilha.
+    int origem[grafo.M]; // Lista contendo os vértices de origem de cada aresta na trilha.
+    int destino[grafo.M]; // Lista contendo os vértices de destino de cada aresta na trilha.
+    int trilha[grafo.V]; // Lista contendo a ordem dos vértices na trilha.
     string mensagem; // String que retorna algum erro no momento da criação da trilha.
     int RA = 233895; // RA do aluno que deve ser preenchido na função que irá criar.
 
