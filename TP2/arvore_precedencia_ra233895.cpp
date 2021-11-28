31a33b1dedebf48bf024584f49d94b95a75150f5
EnricoPf <65573747+EnricoPf@users.noreply.github.com>
2021-11-28 18:23:42 -0300

    Comentários e caso de erro implementado

--- TP2/arvore_precedencia_ra233895.cpp
+++ TP2/arvore_precedencia_ra233895.cpp
@@ -16,35 +16,48 @@ Neste arquivo voce deve implementar sua rotina de criaçao da trilha
 #define INF 0x3f3f3f3f
 
 using namespace std;
-// rvore_precedencia(grafo.V, grafo.M, grafo.W, grafo, &mensagem, RA, dist, pred)
+
 bool arvore_precedencia(int n, int m, int W, Grafo g, string &mensagem, int RA, int *pred, int *dist)
 {
+    //inicializamos o vetor de visitados com o estado inicial de cada vetor sendo não visitado
     vector<bool> visitados(n, false);
+    //colocamos o estado inicial do predecessor de cada nó como não valido e a distância da origem até cada nó como sendo infinito
     for (int i = 0; i < n; i++)
     {
         pred[i] = -1;
         dist[i] = INF;
     }
+    //começamos com a origem da árvore no nó 0, e a distancia de 0 até ele mesmo é 0
     dist[0] = 0;
+    //usamos uma fila de prioridade, ou um heap, para organizar os vértices, em que as operações são de complexidade, no maximo, O(log(V))
     priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> fila;
     fila.push(make_pair(dist[0], 0));
-
+    //Aqui temos uma implementação do algoritmo de Dijsktra
+    //Que itera uma vez em cada vértice e aresta, através do heap
+    //Portanto tem complexidade O((|V| + |E|)* log(V))
     while (!fila.empty())
     {
+        //tiramos o nó que estiver no topo do heap e salvamos seu indice em u
         pair<int, int> top = fila.top();
         int u = top.second;
         fila.pop();
-
+        //Se o vértice ainda não tiver sido visitado, entramos nesse if
         if (visitados[u] == false)
         {
             visitados[u] = true;
 
             list<pair<int, int>>::iterator curr;
-
+            //iteramos entre todos os vértices adjacentes ao vértice atual e damos relax no caminho deles
             for (curr = g.adj[u].begin(); curr != g.adj[u].end(); curr++)
             {
                 int v = (*curr).first;
                 int weight = (*curr).second;
+                //Como essa porção do código passa por todos os vértices alcançaveis por arestas
+                //Fazemos o teste se há algum peso inválido aqui, se ele estiver fora do intervalo (0,W)
+                if ((weight < 0) || (weight > W)){
+                    mensagem = "Erro: Existe peso fora do intervalo.";
+                    return false;
+                }
 
                 //relax (u,v)
                 if (dist[v] > (dist[u] + weight))
@@ -56,5 +69,6 @@ bool arvore_precedencia(int n, int m, int W, Grafo g, string &mensagem, int RA, 
             }
         }
     }
+    //se tudo correu sem erro, retorna true
     return true;
 }
